ab3ac1ef43a2a37694470d50f2f438e7c3ed9de1
Scott Wedge <scott.wedge@gmail.com>
2022-04-17 23:20:14 -0400

    Generate all random values and perform all calculations. The final two secret keys do match!

--- diffie-hellman.py
+++ diffie-hellman.py
@@ -14,6 +14,8 @@
 
 # Example:    Let p=191 and g=2. Suppose Alice picks 42 and Bob picks 33.Then Alice computes 2**42 mod 191 = 20 and Bob computes 2**33 mod 191 =103. They send the results of these computations to each other. Upon receiving 103, Alice computes 103**42 mode 191 = 115, and Bob computes 20**33 mod 191 = 115. So they have a key that they have agreed on without disclosing their secret numbers to each other. They can use this key to communicate securely via a cryptosystem of their choosing. 
 
+# Imports
+import random
 
 # Functions
 def create_prime_list(a, b):
@@ -32,8 +34,63 @@ def create_prime_list(a, b):
 #                    print("   Testing____ {} never == 0".format(j))  #DEBUG
                     primes_list.append(j)  # Add to list of prime numbers
 #                    print(j)  # To display progress to user  #DEBUG
-
     return primes_list
 
 
-print("List of primes is: {}".format(create_prime_list(2,191)))
+def random_prime(l):
+    # Choose a random prime number from list
+    index = random.randint(0, len(l) - 1)
+    return l[index]
+
+
+def random_small_number(min, max):
+    # Return a small number between min and max for value 'g'
+    g = random.randint(min, max)
+    return g
+
+def random_secret_number(min, max):
+    # Return a number between min and max for secret 'n' or 'm'
+    g = random.randint(min, max)
+    return g
+
+
+def a_to_b_mod_c(a, b, c):
+    result = a ** b % c
+    return result
+
+
+prime_list = create_prime_list(151, 191)
+print("List of primes is: {}".format(prime_list))
+
+p = random_prime(prime_list)
+print()  # Blank spacer line
+print("Random prime value from list (for modulo calculation) is {}.".format(p))
+
+g = random_small_number(2,4)
+print()  # Blank spacer line
+print("Random small number (used as base of exponentiation) is: {}.".format(g))
+
+n = random_small_number(30, 50)
+print()  # Blank spacer line
+print("Random small number 'n' (used as exponent) is: {}.".format(n))
+
+m = random_small_number(30, 50)
+print()  # Blank spacer line
+print("Random small number 'm' (used as exponent) is: {}.".format(m))
+
+g_to_n_mod_p = a_to_b_mod_c(g, n, p)
+print()  # Blank spacer line
+print("g to the n mod p (send value to peer) value= {}.".format(g_to_n_mod_p))
+
+g_to_m_mod_p = a_to_b_mod_c(g, m, p)
+print()  # Blank spacer line
+print("g to the m mod p (send value to peer) value= {}.".format(g_to_m_mod_p))
+
+secret_1 = a_to_b_mod_c(g_to_n_mod_p, m, p)
+print()  # Blank spacer line
+print("Secret value 1 = {}.".format(secret_1))
+
+secret_2 = a_to_b_mod_c(g_to_m_mod_p, n, p)
+print()  # Blank spacer line
+print("Secret value 2 = {}.".format(secret_2))
+
